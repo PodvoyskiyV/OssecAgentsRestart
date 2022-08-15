d6463532d4cd8e9e550e4f974b396571df0e08b9
Vadim Podvoyskiy <vadim.podvoyskiy@gmail.com>
2022-08-15 08:21:30 +0500

    Added comments explaining all blocks of code used in this script

--- OssecAgentsRestart.py
+++ OssecAgentsRestart.py
@@ -1,13 +1,20 @@
+#!/bin/python3
+# Read about this script you can at https://github.com/PodvoyskiyV/OssecAgentsRestart
+
+# import necessary modules
 import os
 from pyfiglet import figlet_format
 from termcolor import colored
 
+# create banner
 banner = figlet_format('AgentsRestart')
 print(colored(banner, 'green'))
 
+# create, fill and open the file where we will save active agents
 os.system("/var/ossec/bin/agent_control -lc >> /scripts/active_agents.txt")
 active_agents = open("/scripts/active_agents.txt", 'r')
 
+# loop for restart all active agents
 i = 0
 for active_agent in active_agents:
     if i < 2:
@@ -22,6 +29,6 @@ for active_agent in active_agents:
         os.system(command)
     i += 1
 
+# close and remove the file with active agents
 active_agents.close()
 os.system("rm -f /scripts/active_agents.txt")
-
